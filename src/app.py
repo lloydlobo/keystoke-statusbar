ab534444866eb92c87114eb65958c194715e8bc6
Lloyd Lobo <lloydlobo4@gmail.com>
2023-06-05 14:32:51 +0530

    perf: speed world merge with list enumeration
    - Refactor get_wpm out of class, seems uneccessary.
    - Add function return types.

--- src/app.py
+++ src/app.py
@@ -10,30 +10,42 @@ Inspiration: https://github.com/petternett/railway-statusbar
 import threading
 import time
 import random
-from typing import List, Literal, Optional, Union
-from pynput import keyboard
-# from emoji import emojize
 
-WIDTH = 12
-PLAYER_POSITION = 3  # [..][..][..][v.][RAIL_CHAR]....
+from typing import List, Union  # Literal, Optional
 
-FPS = 30
-FRAME_DELAY = 1.0 / FPS
-MAX_SPEED = 1
-FRICTION_CONST = 0.8
+from pynput import keyboard  # from emoji import emojize
 
-RAIL_CHAR = '..'
-PLAYER_CHAR = '#'
-FIRE_CHAR = '`'
-CACTUS_CHAR = '|'
-HILL_CHAR = '~'
+WIDTH: int = 12
+PLAYER_POSITION: int = 3  # [..][..][..][v.][RAIL_CHAR]....
 
-MAX_HILL_COUNT = 3
-PARA_CONST = 9
-MAX_PARA_ELEMENTS = 2
+FPS: int = 30
+FRAME_DELAY: float = 1.0 / FPS
+MAX_SPEED: int = 1
+FRICTION_CONST: float = 0.8
 
-WPM_CHARS_PER_ROUND = 200
-WPM_AVERAGE_WORD_LEN = 5
+RAIL_CHAR: str = '..'
+PLAYER_CHAR: str = '#'
+FIRE_CHAR: str = '`'
+TREE_CHAR: str = '|'
+HILL_CHAR: str = '~'
+
+MAX_HILL_COUNT: int = 3
+PARA_CONST: int = 9
+MAX_PARA_ELEMENTS: int = 2
+
+WPM_CHARS_PER_ROUND: int = 200
+WPM_AVERAGE_WORD_LEN: int = 5
+
+
+def get_wpm(len_entries, time_start, time_end) -> float:
+    if time_start is not None and time_end is not None:
+        elapsed_time = time_end - time_start
+        minutes = elapsed_time / 60
+        words = len_entries / WPM_AVERAGE_WORD_LEN
+        return round(words / minutes, 2)
+    else:
+        raise ValueError("Invalid `wpm_timer_start` or `wpm_timer_end` \
+                         values. Expected `float` found `None`.")
 
 
 class App:
@@ -67,25 +79,18 @@ class App:
         self.foreground: List[Union[str, None]] = [None] * WIDTH
         self.background: List[Union[str, None]] = [None] * WIDTH
 
-    def get_wpm(self, len_entries):
-        if self.wpm_timer_start is not None and self.wpm_timer_end is not None:
-            elapsed_time = self.wpm_timer_end - self.wpm_timer_start
-            minutes = elapsed_time / 60
-            words = len_entries / WPM_AVERAGE_WORD_LEN
-            return round(words / minutes, 2)
-        else:
-            raise ValueError("Invalid `wpm_timer_start` or `wpm_timer_end` \
-                             values. Expected `float` found `None`.")
-
-    def render(self):
-        # Merge bacground and foreground to compose world.
-        self.world = [
-            x if x is not None else RAIL_CHAR for x in self.background]
-        for i, value in enumerate(self.foreground):
-            if value is not None:
-                self.world[i] = value  # elif self.world[i] is None:
-                #     self.world[i] = RAIL_CHAR
+    # - Merge background and foreground to compose world.
+    #   - Assign values from self.background. Assign `RAIL_CHAR` if None.
+    #   - Assign values from self.foreground. Keeps existing if None.
+    # - Collect data to calculate wpm.
+    # - Concatenate a single buffer with message modules and print per frame.
+    def render(self) -> None:
+        self.world = [bg if bg is not None else RAIL_CHAR
+                      for bg in self.background]
+        self.world = [fg if fg is not None else self.world[i]
+                      for i, fg in enumerate(self.foreground)]
         self.world[PLAYER_POSITION] = PLAYER_CHAR
+
         if self.velocity > 0.9:
             self.world[PLAYER_POSITION - 1] = FIRE_CHAR
             if self.fire_disp % 3 == 0 or self.fire_disp % 2 == 0:
@@ -105,7 +110,8 @@ class App:
 
         if self.wpm_timer_end is not None:
             # self.wpm_timer_end = time.time() # HACK: Set timer twice?
-            self.curr_round_wpm = self.get_wpm(WPM_CHARS_PER_ROUND)
+            self.curr_round_wpm = get_wpm(
+                WPM_CHARS_PER_ROUND, self.wpm_timer_start, self.wpm_timer_end)
             self.wpm_timer_start = None
             self.wpm_timer_end = None
 
@@ -132,7 +138,7 @@ class App:
         if self.debug_text:
             print(f"DEBUG: {self.debug_text}")
 
-    def on_release(self, key):
+    def on_release(self, key) -> None:
         assert isinstance(
             self.new_press_event, threading.Event
         ) or (
@@ -157,7 +163,16 @@ class App:
             self.listener_paused = not self.listener_paused
             # return False # Stop listener.
 
-    def run(self):
+    def debug(self, text: str) -> None:
+        self.debug_text = text
+
+    def pause_listener(self) -> None:
+        self.listener_paused = True
+
+    def resume_listener(self) -> None:
+        self.listener_paused = False
+
+    def run(self) -> None:
         accelaration = 0.0
         counter = 0.0
         para = 0
@@ -204,7 +219,7 @@ class App:
                 self.foreground.pop(0)
                 # `//` gets upper-bound(largest integer) floor division result.
                 if random.randint(0, FPS // 2) == 1:
-                    self.foreground.append(CACTUS_CHAR)
+                    self.foreground.append(TREE_CHAR)
                 else:
                     self.foreground.append(None)
 
@@ -230,15 +245,6 @@ class App:
             self.render()
 
             time.sleep(curr_time + FRAME_DELAY - time.time())
-
-    def debug(self, text):
-        self.debug_text = text
-
-    def pause_listener(self,):
-        self.listener_paused = True
-
-    def resume_listener(self,):
-        self.listener_paused = False
 
 
 if __name__ == "__main__":
@@ -276,7 +282,4 @@ if __name__ == "__main__":
     # world = [x if x is not None else RAIL_CHAR for x in self.background]
     # world = [value if value is not None else world[i] for i, value
     #          in enumerate(self.foreground)]
-
-
-
 """
